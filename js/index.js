4887d7076e988d5d07a4a3fcecd79693a43b79d8
bright okike <brightokike@gmail.com>
2019-10-30 15:48:35 +0300

    the addbooktolibrary function implemented

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
 
-function Book(title, author, pages, publisher, isnb) {
+function Book(title, author, pages, publisher, isnb, image) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.publisher = publisher;
     this.isnb = isnb;
+    this.image = image;
 }
 
 function addBookToLibrary() {
@@ -14,16 +15,14 @@ function addBookToLibrary() {
     let pages = document.getElementById('pages').value;
     let publisher = document.getElementById('publisher').value;
     let isbn = document.getElementById('isnb').value;
-    let myBook = new Book(title, author, pages, publisher, isbn);
+    let image = document.getElementById('image').value;
+    let myBook = new Book(title, author, pages, publisher, isbn, image);
     myLibrary.push(myBook)
-    myLibrary.push("hello")
-    // console.log(myBook)
-    console.log(myLibrary.length)
+    console.log(myLibrary)
 }
 
-let form = document.querySelector('form');
-form.addEventListener('submit', function (e) {
+let form = document.querySelector('.submit');
+form.addEventListener('click', (e) => {
     e.preventDefault;
     addBookToLibrary()
 })
-
